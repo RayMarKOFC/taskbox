c61e08cbf66e6d03255273f8e9330b7bff342721
Ray Martino <raymond.martino@kofc.org>
2021-12-14 11:35:52 -0500

    Create Task component

---
+++ src/app/components/task.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: 'task.component.html'
+})
+export class TaskComponent {
+  @Input() task: Task;
+
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+
+  /**
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+
+  /**
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
